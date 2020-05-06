3060ec95f38c65510ee6ab06e50cc1a49a866e2f
StefanMelich <melich.stefan@centrum.sk>
2020-05-06 23:12:16 +0200

    React Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 // Creates const called render that references react-dom's render function.
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
