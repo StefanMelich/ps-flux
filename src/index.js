01f2d4dc3a2ed6f514d3a9869e9dc043f577ad3e
StefanMelich <melich.stefan@centrum.sk>
2020-05-06 09:55:08 +0200

    Courses page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-//import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+// Creates const called render that references react-dom's render function.
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('root'));
